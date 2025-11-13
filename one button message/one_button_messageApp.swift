48860b92e6196f1997e654298a19c909f7c7227d
Murali <muraliramdj@icloud.com>
2025-11-13 21:25:34 +0530

    Initial Commit

---
+++ one button message/one_button_messageApp.swift
@@ -0,0 +1,17 @@
+//
+//  one_button_messageApp.swift
+//  one button message
+//
+//  Created by Muraliraman on 13/11/25.
+//
+
+import SwiftUI
+
+@main
+struct one_button_messageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
